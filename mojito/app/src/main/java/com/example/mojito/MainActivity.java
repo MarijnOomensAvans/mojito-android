3a557db436483f5ff07f8c6135c680667826c257
MarijnOomensAvans <38461006+MarijnOomensAvans@users.noreply.github.com>
2020-03-23 22:40:01 +0100

    :sparkles: API call to coktaildb working

--- mojito/app/src/main/java/com/example/mojito/MainActivity.java
+++ mojito/app/src/main/java/com/example/mojito/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.mojito;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fetchData();
     }
+
+    private void fetchData() {
+        final TextView textView = findViewById(R.id.text_view);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://www.thecocktaildb.com/api/json/v2/9973533/popular.php";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+
+
+
 }
