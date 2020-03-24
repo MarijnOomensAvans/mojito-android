ec89f2420d2359886c880ff2ded0c1b048c1d773
MarijnOomensAvans <38461006+MarijnOomensAvans@users.noreply.github.com>
2020-03-24 16:27:46 +0100

    :sparkles: Splash screen )))))

---
+++ mojito/app/src/main/java/com/example/mojito/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.mojito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
