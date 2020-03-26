c3522c0be75079cb5002729a645528ce3582d268
MarijnOomensAvans <38461006+MarijnOomensAvans@users.noreply.github.com>
2020-03-26 18:09:28 +0100

    :art: Image in detail

--- mojito/app/src/main/java/com/example/mojito/CocktailDetailFragment.java
+++ mojito/app/src/main/java/com/example/mojito/CocktailDetailFragment.java
@@ -1,10 +1,13 @@
 package com.example.mojito;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +18,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -22,12 +26,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class CocktailDetailFragment extends Fragment {
     public static final String TAG = "CocktailDetailFragment";
 
     private int id;
     private String name;
     private String instructions;
+    private String image;
 
     @Nullable
     @Override
@@ -57,6 +67,7 @@ public class CocktailDetailFragment extends Fragment {
                     JSONObject cocktail = response.getJSONArray("drinks").getJSONObject(0);
                     name = cocktail.getString("strDrink");
                     instructions = cocktail.getString("strInstructions");
+                    image = cocktail.getString("strDrinkThumb");
 
                     initialiseView(view);
                 } catch (JSONException e) {
@@ -79,6 +90,30 @@ public class CocktailDetailFragment extends Fragment {
 
         TextView instructionsText = view.findViewById(R.id.detail_instructions);
         instructionsText.setText(instructions);
+
+        loadImage(view);
+    }
+
+    private void loadImage(View view) {
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+
+        final ImageView mImageView;
+        mImageView = view.findViewById(R.id.detail_image);
+
+        ImageRequest request = new ImageRequest(image,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        mImageView.setImageBitmap(bitmap);
+                    }
+                }, 500, 500, null,
+                new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        mImageView.setImageResource(R.drawable.ic_launcher_foreground);
+                    }
+                });
+
+        queue.add(request);
     }
 
 }
